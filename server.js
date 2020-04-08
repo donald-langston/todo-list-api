b4218f9fb7f7a1ced03920d815d20da2ecf9890f
James Hernandez <james@j-hernandez.com>
2020-04-07 21:36:39 -0400

    feat(server): implements get and post endpoints

--- server.js
+++ server.js
@@ -5,23 +5,50 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var todoList = [
-    {
-        id: 1,
-        todo: "Implement a REST API"
-    }
-];
+var todoList = [{
+    id: 1,
+    todo: "Implement a REST API"
+}, { id: 12, todo: "Implemented" }];
+
 
 // GET /api/todos
+app.get('/api/todos', function(req, res, next) {
+    res.json(todoList)
+})
 
 // GET /api/todos/:id
 
-// POST /api/todos
+app.get('/api/todos/:id', function(req, res, next) {
+    let id = parseInt(req.params.id);
+    let foundID = todoList.find(function(item) {
+        return item.id === id;
+    })
+    res.send(foundID);
+})
 
-// PUT /api/todos/:id
+// POST /api/todos
+app.post('/api/todos', function(req, res, next) {
+        //Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+        let nextId = todoList.reduce((acc, element) => {
+            if (element.id > acc) {
+                return element.id
+            }
+            return acc++
+        }, 0) + 1
+
+        let toDoObj = {
+            id: nextId,
+            todo: newBody.todo
+        }
+        todoList.push(toDoObj)
+
+        res.send(toDoObj)
+
+    })
+    // PUT /api/todos/:id
 
 // DELETE /api/todos/:id
 
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log('Todo List API is now listening on port 3000...');
 })
